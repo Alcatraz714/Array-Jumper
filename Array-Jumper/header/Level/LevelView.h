140f5d709b40a4bf38a57e814703b6beb266c004
Rishi Saxena <rishi.saxena@gamestacy.com>
2024-11-11 14:25:00 +0530

    Added level tiles all

--- Array-Jumper/header/Level/LevelView.h
+++ Array-Jumper/header/Level/LevelView.h
@@ -32,6 +32,9 @@ namespace Level
 		UI::UIElement::ImageView* getBoxOverlayImage(BlockType block_type);
 		void drawBox(sf::Vector2f position);
 		void drawBoxValue(sf::Vector2f position, BlockType box_value);
+		void calculateBoxWidthHeight();
+		void calculateBoxSpacing();
+		sf::Vector2f calculateBoxPosition(int index);
 
 	public:
 		LevelView(LevelController* controller);
